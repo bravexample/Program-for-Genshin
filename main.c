53c4650b9146fe9bfe9d4ccdb6da97269312c236
bravexample <106252820+bravexample@users.noreply.github.com>
2023-02-17 03:26:48 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+#include "program.h"
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	PROGRAM_setup();
+
+	SDL_Event event;
+	int state = 0, tempState = -1;
+
+	do {
+		Sleep(35);
+	
+		if (GetKeyState('V')) 
+			state = 1;
+		else if (GetKeyState('N')) 
+			state = 2;
+		else 
+			state = 0;
+
+		if (state != tempState) {
+			PROGRAM_render(state);
+			tempState = state;
+		}
+	
+		if (state == 1) {
+			mouse_click('l');
+			Sleep(400);
+			key_press('R');
+			Sleep(35);
+			key_press('R');
+		}
+		else if (state == 2) 
+			mouse_click('l');
+	
+		SDL_PollEvent(&event);
+	} while (event.type != SDL_QUIT);
+
+	PROGRAM_clean();
+	return 0;
+}

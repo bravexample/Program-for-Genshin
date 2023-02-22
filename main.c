6d802f04dcd095de78b040bb486560af5236e336
bravexample <106252820+bravexample@users.noreply.github.com>
2023-02-22 23:28:40 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,21 +5,21 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance,
                    _In_ LPSTR lpCmdLine,
                    _In_ int nShowCmd)
 {
-    (void )hPrevInstance; (void )lpCmdLine;
+	(void )hPrevInstance; (void )lpCmdLine;
 
-    Setup(hInstance, nShowCmd);
+	Setup(hInstance, nShowCmd);
 
-    threadState = 1;
+	threadState = 1;
 	HANDLE thread = CreateThread(NULL, 0, ThreadProc, NULL, 0, NULL);
 
 	MSG msg = {0};
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
-    WaitForSingleObject(thread, INFINITE);
+	WaitForSingleObject(thread, INFINITE);
 
-    return (int )msg.wParam;
+	return (int )msg.wParam;
 }
